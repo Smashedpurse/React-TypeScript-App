44f10f84c436d343dcc1ac315e8aafb2b8bce772
Ivan Rubio <smashedpurse@gmail.com>
2023-03-08 12:06:43 -0600

    Use .map to iterate all the info of the array of objects into the useState and provide a style to each one

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,38 @@ import './App.css'
 
 function App() {
 
-  const [number, setNumber] = useState <number | String> (5)
+  const [subs, setSubs] = useState ([
+    {
+      nick:"Dapelu",
+      subMonths:3,
+      avatar: 'https://i.pravatar.cc/150?u=dapelu',
+      description:"Dapelu hace de moderador a veces"
+    },
+    {
+      nick:"Sergio_Serrano",
+      subMonths:7,
+      avatar: 'https://i.pravatar.cc/150?u=sergio'
+    }
+  ])
 
-  const changeNumber = ( ) =>{
-    setNumber("2")
-  }
+
 
   return (
     <div className="App">
-        {number}
-        <button onClick={changeNumber}> Change Number </button>
+        <h1>Midu Subs</h1>
+        <ul>
+          {
+            subs.map(sub =>{
+              return (
+                <li key={sub.nick}>
+                  <img src={sub.avatar} alt={`Avatar for ${sub.nick}`}/>
+                  <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                  <p>{sub.description?.substring(0,100)}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
     </div>
   )
 }
